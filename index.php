ec9ed53ad8197cce6e203651e7fe981924d4ed62
sudhanshuRathore <40271968+sudhanshuRathore@users.noreply.github.com>
2018-06-14 21:16:46 +0530

    Update index.php
    Add comments and fixed a minor pageview display bug.

--- index.php
+++ index.php
@@ -1,36 +1,39 @@
 <?php
 /*
 
-Coded by AeroPwn, pls no steal kthx
-MIT Licensed btw
-pls credit thx
+    ** Code By AeroPwn : http://www.youtube.com/channel/UC0AMc9vyMz4Tko_auIxGmIQ
+    
+    Original Code by TheMindSpeaks : http://themindspeaks.com
 
 */
 
-$db = mysqli_connect('127.0.0.1','root','password','database_name');
+$db = mysqli_connect('127.0.0.1','root','','themindspeaks');
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+    exit();// Exits script if an error is encounted while establishing mysql connection
 }
 
 $datelol = date("Y-m-d");    // Returns YEAR-MONTH-DAY
-$query = "SELECT * FROM `stats` WHERE `date` = '$datelol'";
-$query2 = mysqli_query($db, "SELECT * FROM `stats`");
+$query = "SELECT * FROM `stats` WHERE `date` = '$datelol'";//Query for selection for current date
 
-$result = mysqli_query($db, $query);
-$result2 = mysqli_num_rows($result);
 
-$row = mysqli_fetch_assoc($query2);
+$result = mysqli_query($db, $query);// Execute single result query
+$numRow = mysqli_num_rows($result);
 
-if ($result2 == 0){
+
+
+if ($numRow == 0){// Inserts a new row if stats for current date doesnt exits
     $insertQuery = "INSERT INTO `stats` (`date`) VALUE ('$datelol')";
     mysqli_query($db, $insertQuery);
 }
 
-else {
+else {// Updates page view for current date if row exists
     $updateQuery = "UPDATE `stats` SET `page_views` = `page_views`+1 WHERE `date` = '$datelol'";
     mysqli_query($db, $updateQuery);
 }
+
+$pageView mysqli_query($db, $query);// Seelcts todays page views
+$row = mysqli_fetch_assoc($pageView);// Extracts result
 ?>
 
 <html>
